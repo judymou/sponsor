31c34ecda555f8377d8197444ba7ba0c87b5f373
Judy Mou <moujudy@gmail.com>
2016-02-27 15:24:41 -0800

    add claim model

---
+++ models/Claim.js
@@ -0,0 +1,28 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * Claim Model
+ * ==========
+ */
+
+var Claim = new keystone.List('Claim', {
+	map: { name: 'eventtitle' },
+	autokey: { path: 'slug', from: 'name,sponsor', unique: true }
+});
+
+Claim.add({
+	name: { type: String},
+	email: { type: String},
+  amount: {type: String},
+  eventtitle: {type: String},
+  eventdesp: {type: String},
+  eventlink: {type: String},
+  taxid: {type: String},
+  sponsor: {type: String},
+  state: { type: String}
+});
+
+Claim.defaultColumns = 'name, state|20%, eventtitle|20%, eventdesp|20%, sponsor|20%';
+
+Claim.register();
